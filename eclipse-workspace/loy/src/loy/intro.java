a38063bb83381ed3d5dab869d7d0228dfeaa29ad
Dairo Taiwo Ayo <Dairo Taiwo Ayo@Dairo>
2018-09-15 21:23:23 +0100

    Initialization of first programme

---
+++ eclipse-workspace/loy/src/loy/intro.java
@@ -0,0 +1,13 @@
+package loy;
+
+public class intro {
+
+	public static void main(String[] args) {
+		String firstName, secondName;
+		firstName="OLUWASEGUN"; secondName="OGUNDEJI";
+		
+		System.out.print(firstName+secondName);
+
+	}
+
+}
